e302d7b6a265235864941cfab8ac174d77af87dc
darang22 <skap.mks@gmail.com>
2018-05-07 10:44:21 -0500

    Se agrega el jar de jAspect y el paquete de aspect

---
+++ src/java/org/proyecto/reservas/aspectos/Logging.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.proyecto.reservas.aspectos;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.After;
+
+
+/**
+ *
+ * @author skp
+ */
+public class Logging {
+    
+}
