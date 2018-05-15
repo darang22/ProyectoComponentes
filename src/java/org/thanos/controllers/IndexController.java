8b04a4d9eb41e195078adeb9e010be7e0d182301
jArboleda <jArboleda>
2018-05-15 15:14:52 -0500

    add context

--- src/java/org/thanos/controllers/IndexController.java
+++ src/java/org/thanos/controllers/IndexController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.tahons.controllers;
+package org.thanos.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
